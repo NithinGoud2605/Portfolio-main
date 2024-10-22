ca783b5638f78820c9850fe75f4d0617f472bb63
NithinGoud2605 <sainithingoud007@gmail.com>
2024-10-22 16:30:15 -0400

    add

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,37 @@
+import Button from '../components/Button.jsx';
+import HeroImage from '../../public/assets/final-ai-brush-removebg-4yhtxlg.png';
+
+const Hero = () => {
+  return (
+    <section className="min-h-screen w-full flex flex-col relative pt-20" id="home">
+      <div className="w-full mx-auto flex flex-col sm:mt-12 mt-20 c-space gap-3">
+        <p className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans">
+          Hi,I am 
+        </p>
+        <p className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans">
+          SAI NITHIN GOUD K
+        </p>
+        <p className="hero_tag text-gray_gradient text-2xl  ">FULL STACK DEVELOPER</p>
+      </div>
+
+      <div className="col-span-1 xl:row-span-3 h-full relative">  {/* Added relative here */}
+        <div className="card flex flex-col justify-end h-full">
+      
+          <img src={HeroImage} alt="Hero image" className="object-bottom" />
+
+        </div>
+      </div>
+
+      <div className="w-full h-full absolute inset-0">
+      </div>
+
+      <div className="absolute bottom-20 left-0 right-0 w-full z-10 c-space flex justify-center"> 
+        <a href="#about" className="w-fit">
+          <Button name="Know about me" />
+        </a>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ src/sections/Navbar.jsx
@@ -0,0 +1,62 @@
+import { useState } from 'react';
+
+import { navLinks } from '../constants/index.js';
+import logo from '../../public/assets/logo.png';
+import Resume from '../../public/assets/Resume.pdf';
+
+const NavItems = ({ onClick = () => {} }) => (
+  <ul className="nav-ul">
+    {navLinks.map((item) => (
+      <li key={item.id} className="nav-li">
+        <a href={item.href} className="nav-li_a" onClick={onClick}>
+          {item.name}
+        </a>
+      </li>
+    ))}
+  </ul>
+);
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => setIsOpen(!isOpen);
+  const closeMenu = () => setIsOpen(false);
+
+  return (
+    <header className="fixed top-0 left-0 right-0 z-50 bg-black/90">
+      <div className="max-w-7xl mx-auto">
+        <div className="flex justify-between items-center py-5 mx-auto c-space">
+          <a href="/">
+            <img src={logo} alt="logo image" className="w-24 h-auto" />
+          </a>
+
+          <button
+            onClick={toggleMenu}
+            className="text-neutral-400 hover:text-white focus:outline-none sm:hidden flex"
+            aria-label="Toggle menu">
+            <img src={isOpen ? 'assets/close.svg' : 'assets/menu.svg'} alt="toggle" className="w-6 h-6" />
+          </button>
+
+          <nav className="sm:flex hidden">
+            <NavItems />
+
+          </nav>
+          <a
+              href={Resume}
+              className="text-black bg-white ml-4 shadow-lg p-3 rounded transition-transform duration-200 hover:scale-105"
+              download>
+              Resume
+            </a>
+        </div>
+      </div>
+
+      <div className={`nav-sidebar ${isOpen ? 'max-h-screen' : 'max-h-0'}`}>
+        <nav className="p-5">
+          <NavItems onClick={closeMenu} />
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
